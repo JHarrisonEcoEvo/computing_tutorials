f4b3182f6af19740e90ce33a66c59c82be346861
Joshua Harrison <joshua.grant.harrison@gmail.com>
2023-11-06 17:59:02 -0800

    first commit

---
+++ sklearn_playing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn import tree
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_wine
+
+wines = load_wine(as_frame=True)
+
+# This is the series of target values
+wines.target
+
+# This is the feature array
+wines.data
+
+# Do some simple EDA here.
+wines.data.shape
+
+# Are there any NAs in any feature?
+wines.data.columns
+np.count_nonzero(np.isnan(wines.data), axis=0)
+
+# What is the distribution of data for each feature?
+df = wines.data
+df.plot(subplots=True)
+
+# How balanced are the classes? Fairly balanced
+pd.DataFrame(wines.target).value_counts()
+
+# These are on pretty different scales. If we end up doing a regression we should conver them to z scores to compare
+# beta coefficeints.
+# For classification without a lot of feature interrogation, we can use the data as they are.
+
+# Lets do a 70/30 test/train split
+x_train, x_test, y_train, y_test = train_test_split(
+    df, wines.target, test_size=0.3, shuffle=True, random_state = 66)
+
+print(x_train.shape)
+print(x_test.shape)
+
+model = ExtraTreesClassifier()
+model.fit(x_train, y_train)
+
+#predict
+predicted = model.predict(x_test)
+
+print(metrics.classification_report(y_test, predicted, target_names=wines.target_names))
+print(metrics.confusion_matrix(y_test, predicted))
